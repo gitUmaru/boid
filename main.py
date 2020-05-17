061c492e372187ba991afda3285fa443c64eb01f
umar.ali <umaru.alia@gmail.com>
2020-05-17 00:27:02 -0700

    cone()

--- main.py
+++ main.py
@@ -6,17 +6,8 @@ def setup():
     background(204)
 
 def draw():
-    if mouse_is_pressed:
-        fill(random_uniform(255), random_uniform(127), random_uniform(51), 127)
-    else:
-        fill(255, 15)
+    cone()
 
-    circle_size = random_uniform(low=10, high=80)
-
-    circle((mouse_x, mouse_y), circle_size)
-
-def key_pressed(event):
-    background(204)
 
 if __name__ == '__main__':
     run()
